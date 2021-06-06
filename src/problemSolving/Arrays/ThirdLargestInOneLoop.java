04750992a9d47d5b2d01a044492c4724b8d447dc
Vikas6206 <vikas.kr6206@gmail.com>
2021-06-07 02:25:22 +0530

    Added the O(n) complexity for third largest element

--- src/problemSolving/Arrays/ThirdLargestInOneLoop.java
+++ src/problemSolving/Arrays/ThirdLargestInOneLoop.java
@@ -2,6 +2,26 @@ package problemSolving.Arrays;
 
 public class ThirdLargestInOneLoop {
     public static void main(String args[]){
+        int arr[]={3,17,19,4,7,23,87};
 
+        int firstLargest=0,secondLargest=0,thirdLargest=0;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>firstLargest){
+                thirdLargest=secondLargest;
+                secondLargest=firstLargest;
+                firstLargest=arr[i];
+
+            }else if( arr[i]>secondLargest && arr[i]!=firstLargest){
+                thirdLargest=secondLargest;
+                secondLargest=arr[i];
+            }else if(arr[i]>thirdLargest && arr[i]!=secondLargest && arr[i]!=firstLargest){
+                thirdLargest=arr[i];
+            }
+        }
+
+        System.out.println("Third largest :"+thirdLargest);
+        System.out.println("Second largest :"+secondLargest);
+        System.out.println("First largest :"+firstLargest);
     }
 }
